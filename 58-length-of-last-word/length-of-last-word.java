4af6212b1f588c9aaaa875e0165b286b2c6a9007
Karan Gupta <karangupta5212@gmail.com>
2025-09-07 04:03:35 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (65.96%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int end= s.length()-1;
+        while(end>=0 && s.charAt(end)==' '){
+            end--;
+        }
+        int count=0;
+        while(end>=0 && s.charAt(end)!=' '){
+            count++;
+            end--;
+        }
+        return count;
+        
+
+    }
+}
